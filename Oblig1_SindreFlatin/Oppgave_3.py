20a00748c90cbcbad195e8c274abc58fb1eb8769
dSindrus <sindreflatin@gmail.com>
2022-09-05 08:58:01 +0200

    Create Oppgave_3.py

---
+++ Oblig1_SindreFlatin/Oppgave_3.py
@@ -0,0 +1,16 @@
+# Lag en mini-kalkulator. Ta to tall som input fra brukeren på en fornuftig måte.
+#Basert på de to tallene, skriv ut resultatet av samtlige av de følgende operatorene
+# (Det holder med å skrive ut alle operasjonene etter hverandre, men det er jo alltids lov å gjøre det mer komplisert hvis man vil):
+
+# * (gange)
+# / (dele)
+# + (pluss)
+# - (minus)
+# % (modulo)
+# ** (opphøye)
+# // (dele med nedrunding)
+
+tall_1 = int(input())
+tall_2 = int(input())
+
+print((tall_1) + (tall_2))
