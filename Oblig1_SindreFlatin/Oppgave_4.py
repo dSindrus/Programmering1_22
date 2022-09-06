51aefba2f7b38a47d4b3b6418c24765f377de5cc
dSindrus <sindreflatin@gmail.com>
2022-09-06 16:55:03 +0200

    .

--- Oblig1_SindreFlatin/Oppgave_4.py
+++ Oblig1_SindreFlatin/Oppgave_4.py
@@ -1,15 +1,15 @@
 
 a = 6
-b = 3           #Her gis a-c bestemte verdier, da forøvrig i int.
+b = 3           # Her gis a-c bestemte verdier av typen int.
 c = 2
 print(" ")
 print("Når a = 6, b = 3 og c = 2")          # Enkel definisjon av premissets verdier på de forskjellige variablene.
 
-print ("----------------------------")      # Fullstendig unødvendig, men resultatet blir mer ryddig
+print ("----------------------------")      # Unødvendig, lagt inn for ordensskyld
 
-print(f"a) Er da a+b*c: {a+b*c}")           #\
-print(f"b) Er da (a+b)*c: {(a+b)*c}")       # \
-print(f"c) Er da a/b/c: {a/b/c}")           #  | Her gjøres utregningene
-print(f"d) Er da a/(b/c): {a/(b/c)}")       # /
-                                            #/
-print("----------------------------")#/
+print(f"a) Er da a+b*c: {a+b*c}")
+print(f"b) Er da (a+b)*c: {(a+b)*c}")
+print(f"c) Er da a/b/c: {a/b/c}")           # Her gjøres utregningene gitt i oppgaven med riktige operatorer
+print(f"d) Er da a/(b/c): {a/(b/c)}")
+
+print("----------------------------")       # Unødvendig, lagt inn for ordensskyld
--- Oblig1_SindreFlatin/Oppgave_5.py
+++ Oblig1_SindreFlatin/Oppgave_5.py
@@ -1,10 +1,3 @@
-#//Note to self//
-#Oversikt over hvor mange småkaker 5 personer har spist:
-#Person 1: 5 småkaker
-#Person 2: 9 småkaker
-#Person 3: 2.3 småkaker
-#Person 4: 21 småkaker
-#Person 5: 0 småkaker
 
 person_1 = 5
 person_2 = 9
@@ -13,24 +6,23 @@ person_4 = 21
 person_5 = 0
 antall_personer = 5             # Her defineres totalt antall personer.
 
-antall_kaker = person_1 + person_2 + person_3 + person_4 + person_5     # Her defineres totalt antall kaker spist. kun for å gjøre regnestykket under kortere.
-print("---------------------------------------------------------------------------------------") #unødvendig, lagt inn for ordensskyld
+antall_kaker = person_1 + person_2 + person_3 + person_4 + person_5     # Her defineres totalt antall kaker spist. kun for å gjøre regnestykket under ryddigere.
+print(" ") # Unødvendig, lagt inn for ordensskyld
 print("Oversikt over hvor mange småkaker hver person spiser:")
 
-print(" ")
+print(" ")                      # Lagt inn for å skape mer rom mellom linjene.
 
 print("Person 1 spiser 5 småkaker,")
 print("person 2 spiser 9 småkaker,")
 print("person 3 spiser 2.5 småkaker,")
-print("person 4 spiser 21 småkaker(jeebus),")
+print("person 4 spiser 21 småkaker,")
 print("og person 5 spiser ingen kaker.")
 
-print(" " )
+print(" ")                      # Lagt inn for å skape mer rom mellom linjene.
 
 print(f"Totalt antall småkaker spist er: {antall_kaker}")
 print(" ")
-print(f"Antall småkaker de fem personene spiser i gjennomsnitt på en uke er da: {int(antall_kaker/antall_personer)}")       #Her regnes gjennomsnittet ut ved hjelp av dataypen int, og operatoren /.
-                                                                                                                            # int fjerner desimalene, og runder i praksis ned verdien
-print( )
-print("---------------------------------------------------------------------------------------")                            #Unødvending, lagt inn for ordensskyld
-print(f"Runder vi ikke ned, er antallet i gjennomsnitt: {float(antall_kaker/antall_personer)}")                             #Samme utregning, men ved bruk av float. Ikke nødvendig, men følte jeg måtte. :-)
+print(f"Antall småkaker personene spiser i gjennomsnitt på en uke er da: {int(antall_kaker/antall_personer)}")     # Her regnes gjennomsnittet ut ved hjelp av dataypen int, og operatoren /.
+                                                                                                                    # int fjerner desimalene, og runder i praksis ned verdien
+print("---------------------------------------------------------------------------------------")                    # Unødvending, lagt inn for ordensskyld
+print(f"Runder vi ikke ned, er antallet i gjennomsnitt: {float(antall_kaker/antall_personer)}")                     # Samme utregning, men ved bruk av float. Ikke nødvendig, men følte jeg måtte. :-)
