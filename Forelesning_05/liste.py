605086110fab526ee2861bbe48905ca3f2d13d7f
dSindrus <sindreflatin@gmail.com>
2022-09-08 10:33:49 +0200

    .

---
+++ Forelesning_05/liste.py
@@ -0,0 +1,15 @@
+print(" ")
+
+random_list = ["Europe", 8, 2.4, False, ["Some element", 42]]
+print(random_list)
+
+print(" ")
+
+games = ["Dark Souls", "Disco Elysium", "God of War", "Hunt: Showdown", "Dota 2"]
+print(games)
+
+print(" ")
+
+print(games[2])
+specific_game = games[1]
+print(specific_game)
