58507ab1b91c7e0c939b89e6f56bcf2e97106c10
dSindrus <sindreflatin@gmail.com>
2022-09-13 10:46:54 +0200

    Create loops.py

---
+++ Forelesning_06/loops.py
@@ -0,0 +1,13 @@
+print("")
+
+game_list = ["Disco Elysium", "Hunt: Showdown", "Jump King", "Stardew Valley", "Dark Souls", "Trackmania 2: Stadium", "Counter Strike: Global Offensive"]
+
+for games in game_list:
+    print(f"{games} er et bra spill!" )
+    print("You should try it \n")
+
+print("---------------------------------")
+
+dark_souls = "Dark Souls"
+for character in dark_souls:
+    print(character)
