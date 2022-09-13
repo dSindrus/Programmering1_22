6dddbd417701194862e7d01cf3c17f773e7fe0d8
dSindrus <sindreflatin@gmail.com>
2022-09-13 11:49:32 +0200

    Update games_list_operations.py

--- Forelesning_06/games_list_operations.py
+++ Forelesning_06/games_list_operations.py
@@ -1,8 +1,18 @@
 print("")
 
-game_list = ["Disco Elysium", "Hunt: Showdown", "Jump King", "Stardew Valley",]
+games_list = ["Disco Elysium", "Hunt: Showdown", "Jump King", "Stardew Valley",]
 
 for number_in_series in range(1,4):
-    game_list.append(f"Dark Souls {number_in_series}")              #Legger til hvert spill i serien til listen med spill
+    games_list.append(f"Dark Souls {number_in_series}")              #Legger til hvert spill i serien til listen med spill
 
-print(game_list)
+print(games_list)
+
+print("---------------------------------------------------------------------")
+
+games_without_ds = []
+
+for game in games_list:
+    if "Dark Souls" not in game:                                    #fjerner Dark Souls-spillene fra listen. Sjekker iterativt etter "Dark Souls" i hvert element.
+        games_without_ds.append(game)
+
+print(f"Games without the Dark Souls games: {games_without_ds}")
