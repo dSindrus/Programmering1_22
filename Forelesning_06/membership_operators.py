853afbea62087e38c52f474a9f10d2d3ff98bd4e
dSindrus <sindreflatin@gmail.com>
2022-09-13 10:29:02 +0200

    -

---
+++ Forelesning_06/membership_operators.py
@@ -0,0 +1,17 @@
+print(" ")
+
+game_list = ["Disco Elysium", "Hunt: Showdown", "Jump King", "Stardew Valley", "Dark Souls"]
+
+if "Disco Elysium" in game_list:
+    print("Cool!")
+
+if "Dark Souls" not in game_list:
+    print("Good for you!")
+
+print("-----------------------------------------")
+
+opinion = "I hecking love Battle Royale games!"
+
+if "Battle Royale" in opinion:
+    print("Washed up genre..")
+
