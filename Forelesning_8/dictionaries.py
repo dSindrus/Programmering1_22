f34a2cc8c938bfab415b01cc70a689a8f9ca54c7
dSindrus <sindreflatin@gmail.com>
2022-09-20 11:46:58 +0200

    .

---
+++ Forelesning_8/dictionaries.py
@@ -0,0 +1,53 @@
+print("--------------------------------------------------------------")
+#Fra presentasjon i forelesningen
+movie= {'title': 'Soul','year': 2020, 'duration': 100, 'score':8.4}        #legger til nøkkler med tilhørende verdier til en variabel. Disse kan endres/refereres til
+print(movie['title'])
+print(f"Filmen varer i {movie.get('duration')} minutter")
+
+print("--------------------------------------------------------------")
+
+movie['director'] = 'Pete Docter'                           #Oppretter en ny nøkkel med tilhørende verdi
+
+print(f"Filmen er regissert av {movie.get('director')}")    #henter ut kun nøkkelen vi opprettet over.
+
+print("--------------------------------------------------------------")
+
+#.pop() metoden kan brukes til å fjerne en nøkkel.
+#dictionary_name.pop(nøkkel)
+#del-statement kan også brukes. Den kan slette alt mulig.
+#del dictionary_name[nøkkel]
+
+movie.pop('director')
+del movie['score']
+print(movie)
+
+print("--------------------------------------------------------------")
+print("")
+#Nytt eksempel:
+
+game = {'name': 'Dota 2',
+        'genre': 'MOBA',
+}
+
+print(game['name'])                                         #henter ut kun nøkkelen vi opprettet over.
+print(game['genre'])                                        #henter ut kun nøkkelen vi opprettet over.
+
+print(game.get('Genre'))                                    #Nøkkelen er case-sensitive
+
+print("--------------------------------------------------------------")
+
+game['max players'] = 10                                    #Legger til nøkkel til dictionary'en
+print(game)
+
+print("--------------------------------------------------------------")
+
+game['name'] = 'DOTA 2'                                     #Endrer navn på element/nøkkel
+print(game)
+
+print("--------------------------------------------------------------")
+
+game.pop('max players')                                     #Fjerner et nøkkel-element
+print(game)
+
+del game['genre']
+print(game)                                                 #Annen metode å fjerne nøkkel-element
---
+++ Forelesning_8/tuples.py
@@ -0,0 +1,18 @@
+print("--------------------------------------------------------------")
+#Tuples=statisk liste, men kan hente ut, eller referere til elementer likevel. Ikke endre!
+
+games = ('Dark Souls', 'Disco Elysium', 'Hunt: Showdown', 'Tekken 7', 'Dota 2', 'Hollow Knight', 'Jump King')
+print(games)
+
+print("--------------------------------------------------------------")
+print(games[5])
+print(games[0:3])
+#games[3] = "TEKKEN 8"       #ikke lov i tuples
+#games.append("Tetris")      #ikke lov i tuples
+#games.insert(4, "Tetris")   #ikke lov i tuples
+#games.pop(5)                #ikke lov i tuples
+#games.remove("Jump King")   #ikke lov i tuples
+
+print("--------------------------------------------------------------")
+
+
