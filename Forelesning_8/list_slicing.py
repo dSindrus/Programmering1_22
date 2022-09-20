278ec358c6b3ce73cc6344fdcdf5ee834003703a
dSindrus <sindreflatin@gmail.com>
2022-09-20 10:31:26 +0200

    .

---
+++ Forelesning_8/list_slicing.py
@@ -0,0 +1,12 @@
+print("--------------------------------------------------------------")
+
+games = ["Dark Souls", "Disco Elysium", "Hunt: Showdown", "Tekken 7", "Dota 2", "Hollow Knight", "Jump King"]
+
+print(games[1:5])               #print deler av liste
+print(games[2:4])               #print deler av liste
+
+print("--------------------------------------------------------------")
+
+print(games[1:6:2])             #intervall
+
+print("--------------------------------------------------------------")
