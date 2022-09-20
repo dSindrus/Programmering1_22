a7b736708b5a738b5e58375562193e9d2ef44fb0
dSindrus <sindreflatin@gmail.com>
2022-09-20 10:41:46 +0200

    Update list_slicing.py

--- Forelesning_8/list_slicing.py
+++ Forelesning_8/list_slicing.py
@@ -8,5 +8,16 @@ print(games[2:4])               #print deler av liste
 print("--------------------------------------------------------------")
 
 print(games[1:6:2])             #intervall
+print(games[:])                 #kopierer hele lista
+print(games[2:])                #kopierer elementer fom. index 2
+print(games[:2])                #fra start, men til, men ikke med, index 2
 
 print("--------------------------------------------------------------")
+
+print(games[-2])                #tar utgangspunkt i siste elemnt i liste fra -1 og nedover. Dette er nest siste element.
+print(games[-5:-2])             #====||====
+
+print("--------------------------------------------------------------")
+
+classic_game = "Shadow of the Colussus"
+print(classic_game[10:])        #printer ut del av tekst-strengen. index for hver bokstav.
