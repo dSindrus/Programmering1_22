c693a192ede46f2341d4114db63b15f120bd36c1
dSindrus <sindreflatin@gmail.com>
2022-09-21 10:39:54 +0200

    Update Oppgave6.py

--- Oblig2_SindreFlatin/Oppgave6.py
+++ Oblig2_SindreFlatin/Oppgave6.py
@@ -1,19 +1,13 @@
-#En for å legge til noe, en for å slette noe, og en for å skrive ut hele listen.
-#Brukeren skal deretter kunne skrive en input for å velge hvilken kommando som skal taes i bruk.
-#Programmet skal gå i en evig løkke frem til brukeren avslutter det med en egen kommando for dette.
-
-#"Pakkelisten! Hva vil du gjøre?\n 1. Legge til noe? 2. Fjerne noe? 3. Se lista 4. Avslutte\nSvar her:"
 print("-------------------------------------")
 
 pakkelista = []
 handling = int
 
 while handling != "4":
-    handling = input("Pakkelisten! Hva vil du gjøre?\n1.) Legge til noe? \n2.) Fjerne noe? \n3.) Se lista \n4.) Avslutte.\nSvar her:")
+    handling = input("Pakkelisten! Hva vil du gjøre?\n1) Legge til noe? \n2) Fjerne noe? \n3) Se lista \n4) Avslutte.\nSvar med et tall her:")
     print("------------------------")
     if handling == "1":
         pakkelista.append(input("Hva du legge til i lista?"))
-
 
     if handling == "2":
         print("------------------------")
