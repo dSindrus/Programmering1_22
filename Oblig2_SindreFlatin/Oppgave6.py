806d79e638cbe319c5fd1e8f19e0197b2c150218
dSindrus <sindreflatin@gmail.com>
2022-09-21 11:57:09 +0200

    .

--- Oblig2_SindreFlatin/Oppgave6.py
+++ Oblig2_SindreFlatin/Oppgave6.py
@@ -4,19 +4,20 @@ pakkelista = []
 handling = str
 
 while handling != "4":
-    handling = input("Pakkelisten! Hva vil du gjøre?\n1) Legge til noe? \n2) Fjerne noe? \n3) Se lista \n4) Avslutte.\nSvar med et tall her:")
+    handling = input("Pakkelisten! Hva vil du gjøre?\n1) Legge til noe? \n2) Fjerne noe? \n3) Se lista \n4) Avslutte.\nSvar med et tall her:")          #definerer en input med tekst og brukte \n for å få de listet loddrett
     print("------------------------")
     if handling == "1":
-        pakkelista.append(input("Hva du legge til i lista?"))
+        pakkelista.append(input("Hva du legge til i lista?"))                   #legger til elementer i lista ved hjelp av .apppend.
 
     if handling == "2":
         print("------------------------")
-        print(pakkelista)
-        pakkelista.pop(input("Hva vil du fjerne fra lista?"))
+        print(pakkelista)                                                       #La til en print av listen for oversiktens skyld.
+        pakkelista.remove(input("Hva vil du fjerne fra lista?"))                   #Fjerner det definerte elementet ved hjelp av .pop
 
     if handling == "3":
+        print("PAKKELISTA:")
         print("------------------------")
-        print(pakkelista)
+        print(pakkelista)                                                       #enkel print av lista
         print("------------------------")
 
     if handling == "4":
