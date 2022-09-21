122e5a5ad5473305f3014678657ca0b28f6ff9bf
dSindrus <sindreflatin@gmail.com>
2022-09-22 00:10:24 +0200

    .

--- Oblig2_SindreFlatin/Oppgave1.py
+++ Oblig2_SindreFlatin/Oppgave1.py
@@ -1,13 +1,14 @@
 print("-------------------------------------")
-
-number = input("Hva er meningen med livet?\n (Hint: det er et tall) :")
-number = int(number)        #konverterer input'en
-
-if number == 42:
-    print("Det stemmer, meningen med livet er 42!")
-elif number >= 30 and number <= 50:             #om tallet er større enn, eller lik 30, og mindre enn, eller lik 50
-    print("Nærme, men feil!")
-else:                                           #for alt annet, print "feil!"
-    print("Feil!")
+while True:                                         # Hindrer programmet i å stoppe etter hver input ved hjeløp av en while-løkke
+    number = input("Hva er meningen med livet?\n (Hint: det er et tall) :")
+    number = int(number)        #konverterer input'en
+    if number == 42:
+        print("Det stemmer, meningen med livet er 42!")
+        print("-------------------------------------")
+        exit()
+    elif number >= 30 and number <= 50:             #om tallet er større enn, eller lik 30, og mindre enn, eller lik 50
+        print("Nærme, men feil!")
+    else:                                           #for alt annet, print "feil!"
+        print("Feil!")
 
 print("-------------------------------------")
