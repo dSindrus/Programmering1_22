67d837894cc780f1f8bcbc688c576bf3ddc2332a
dSindrus <sindreflatin@gmail.com>
2022-09-22 11:30:30 +0200

    .

---
+++ digital_forelesning22-09/funksjoner.py
@@ -0,0 +1,9 @@
+print("------------------------------")
+import datetime                             #Henter inn informasjon om dagens dato i python
+
+def print_current_date():
+    current_date = datetime.date.today()
+    print(current_date)
+
+print_current_date()                        #kaller på funksjonen
+
