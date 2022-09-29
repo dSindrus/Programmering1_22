238776d2490b265b8ff4534872ce4349791c7837
dSindrus <sindreflatin@gmail.com>
2022-09-29 11:59:19 +0200

    .

--- Forelesning_11/file_reading.py
+++ Forelesning_11/file_reading.py
@@ -1,3 +1,23 @@
 print("------------------------------------------------")
 
-file = open("zen_of_python.txt")
+'''file = open("zen_of_python.txt")
+print(file.read())
+
+file.close()'''
+
+try:                                                            #Sjekker filnavnet stemmer ved hjel av try og except.
+    with open("text_files/zen_of_python.txt") as file:          #henter og åpner fil ved hjelp av with <åpne fil> as file:.
+        print(file.read())
+except FileNotFoundError:
+    print("File was not found")
+
+print("------------------------------------------------")
+
+with open("text_files/zen_of_python.txt") as file:              #Henter ut hver linje i teskstfilen som et eget element. Vannrett, vel å merke.
+    print(file.readlines())
+
+print("------------------------------------------------")
+
+with open("text_files/zen_of_python.txt") as file:              #Henter ut en linje fra teksten, og starter med første, andre, tredje, osv.
+    print(file.readline().rstrip())                             #.rstrip fjerner tomrom fra tekstfil, eksempelvis mellomrom mellom linjer.
+    print(file.readline().rstrip()) 
---
+++ Forelesning_11/file_writing.py
@@ -0,0 +1,5 @@
+print("------------------------------------------------")
+
+with open("text_files/my_novel.txt", "w") as file:
+    user_input = input(": ")
+    file.write(user_input)
