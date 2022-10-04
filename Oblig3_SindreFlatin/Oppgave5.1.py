fef2085e8cad7b4a73c58e03417314fdd3924e63
dSindrus <sindreflatin@gmail.com>
2022-10-04 17:46:25 +0200

    Update Oppgave5.1.py

--- Oblig3_SindreFlatin/Oppgave5.1.py
+++ Oblig3_SindreFlatin/Oppgave5.1.py
@@ -2,9 +2,9 @@ print("-----------------------------")
 
 #A)
 
-Inception = {"name": "Inception", "year": 2010, "imdb-rating": 8.7}
-InsideOut = {"name": "Inside Out", "year": 2015, "imdb-rating": 8.1}                    #Definerer 3 dictionaries med data om hver sin film
-ConAir = {"name": "Con Air", "year": 1997, "imdb-rating": 6.9}
+Inception = {"name": "Inception", "year": 2010, "rating": 8.7}
+InsideOut = {"name": "Inside Out", "year": 2015, "rating": 8.1}                    #Definerer 3 dictionaries med data om hver sin film
+ConAir = {"name": "Con Air", "year": 1997, "rating": 6.9}
 
 movie_list = [Inception,InsideOut,ConAir]                                               #Oppretter lista med dictionaries
 
@@ -12,23 +12,26 @@ print(movie_list)                                                               
 
 print("-----------------------------")
 
-#B)
+#B) OG C)
 
-Snatch = {"name": "Snatch", "year": 2000, "imdb-rating": 8.2}
-Shawshank = {"name": "The Shawshank Redemption", "year": 1994, "imdb-rating": 9.3}  # Definerer 3 dictionaries med data om hver sin film
-Nobody = {"name": "Mr. Nobody", "year": 2009, "imdb-rating": 7.8}
 
-new_movie_list = [Snatch, Shawshank, Nobody]
+def add_movie(list, name, year, rating=5):
+    movie_list.append({"name": name,"year": year,"rating": rating})
 
-def added_movies():
-    for movies in new_movie_list:
-        movie_list.append(new_movie_list)
 
-added_movies(movie_list)
+add_movie(movie_list, "Snatch", 2000, 8.2)
+add_movie(movie_list, "The Shawshank Redemption", 1994, 9.3)
+add_movie(movie_list, "Mr Nobody", 2009, 7.8)
+add_movie(movie_list, "Kopps", 2003, )                                              #Får automatisk rating 5
+
+print(list(movie_list))
+
+'''for elementer in range(len(movie_list)):
+    for element in movie_list:
+        print(element[elementer])'''
 
 print("-----------------------------")
 
 #C)
-
 '''OPPGAVE 5.1 (C)'''
 
