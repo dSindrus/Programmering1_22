1a5ec8fb517f429dbf0951b76a18a577969f2e2d
dSindrus <sindreflatin@gmail.com>
2022-10-10 13:17:46 +0200

    Update Oppgave1.py

--- Oblig4_SindreFlatin/Oppgave1.py
+++ Oblig4_SindreFlatin/Oppgave1.py
@@ -8,15 +8,23 @@ class Movie:
         self.year = year
         self.rating = rating
 
-
+#B)
     def get_movie(self):
-        return f"{self.title} was released in {self.year}, and currently has a score of {self.rating}"
+        print(f"{self.title} was released in {self.year}, and currently has a score of {self.rating}")
 
 
 Inception = Movie("Inception", 2010, 8.8)
 TheMartian = Movie("The Martian", 2015, 8)
 Joker = Movie("Joker", 2019, 8.4)
 
-Movie.get_movie(Inception)
-Movie.get_movie(TheMartian)
-Movie.get_movie(Joker)
+#A)
+print(f"{Inception.title} was released in {Inception.year} and currently has a score of {Inception.rating}")
+print(f"{TheMartian.title} was released in {TheMartian.year} and currently has a score of {TheMartian.rating}")
+print(f"{Joker.title} was released in {Joker.year} and currently has a score of {Joker.rating}")
+
+print("-----------------------------------------------------------")
+
+#B)
+Inception.get_movie()
+TheMartian.get_movie()
+Joker.get_movie()
