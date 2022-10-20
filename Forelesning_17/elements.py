c24047f51b48532047fa33644b4b9cfc3dd4e6b8
dSindrus <sindreflatin@gmail.com>
2022-10-20 10:34:48 +0200

    .

---
+++ Forelesning_17/elements.py
@@ -0,0 +1,16 @@
+import dearpygui.dearpygui as dpg
+
+dpg.create_context()
+dpg.create_viewport(title="Elementer", width=600, height=400)
+
+with dpg.window(label="window name", width=500, height=300):
+    dpg.add_text("Dette er en statisk tekst!")
+    dpg.add_button(label="Knapp")
+    dpg.add_input_text(label="Tekstfelt", width=150)
+    dpg.add_input_int(label="Skriv inn tall her", width=150)
+    dpg.add_input_float(label="Floatfelt", width=150)
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
