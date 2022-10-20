d9c5d17e65e00e147a25917494997be4b54474b8
dSindrus <sindreflatin@gmail.com>
2022-10-20 10:53:59 +0200

    .

--- Forelesning_17/elements.py
+++ Forelesning_17/elements.py
@@ -5,10 +5,10 @@ dpg.create_viewport(title="Elementer", width=600, height=400)
 
 with dpg.window(label="window name", width=500, height=300):
     dpg.add_text("Dette er en statisk tekst!")
-    dpg.add_button(label="Knapp")
     dpg.add_input_text(label="Tekstfelt", width=150)
-    dpg.add_input_int(label="Skriv inn tall her", width=150)
-    dpg.add_input_float(label="Floatfelt", width=150)
+    dpg.add_input_int(label="Int-felt", width=150)
+    dpg.add_input_float(label="Float-felt", width=150)
+    dpg.add_button(label="Knapp")
 
 dpg.setup_dearpygui()
 dpg.show_viewport()
