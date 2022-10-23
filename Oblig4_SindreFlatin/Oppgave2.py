41992912d13b80d24b29b5cc66f09974498ec9c2
dSindrus <sindreflatin@gmail.com>
2022-10-23 03:15:29 +0200

    .

--- Oblig4_SindreFlatin/Oppgave2.py
+++ Oblig4_SindreFlatin/Oppgave2.py
@@ -8,6 +8,7 @@ player_hand = []
 dealer_hand = []
 player_input = "x"
 chip_stack = 5
+bet = int
 
 def deal_cards():
     player_hand.append(random.choice(list(deck)))
@@ -29,29 +30,33 @@ def deal_cards():
     '''print("-------------------------------")
     print("Welcome to a game of blackjack!")
     print("-------------------------------")'''
-    print(f"The cards have now been dealt. \nYou have a {player_hand[0]} and a {player_hand[1]} with a total value of {player_score}")
+    print(f"The cards have now been dealt. \n-------------------------------\nYou have a {player_hand[0]} and a {player_hand[1]} with a total value of {player_score}")
     print(f"The dealers visible card is a {dealer_hand[0]}, with a value of {bjm.get_card_value(dealer_hand[0])}")
 
 
 def print_results_stand(player_score, dealer_score, chip_stack):
     if dealer_score > 21:
-        chip_stack.append(bet)
         print(f"Your hand is {player_hand} with a value of {player_score}")
         print(f"Dealer's hand is {dealer_hand} with a value of {dealer_score}")
         print("You win!")
-        print(f"You have now {chip_stack} chips")
+        chip_stack = chip_stack + bet
+        print(f"You now have {chip_stack} chips")
+
     if player_score > dealer_score and player_score < 22:                               #Her fant jeg ut at jeg hadde feil tegnsetting etter "and dealer_score"
-        chip_stack.append(bet)
         print(f"Your hand is {player_hand} with a value of {player_score}")
         print(f"Dealer's hand is {dealer_hand} with a value of {dealer_score}")         #hadde feil variabel i printen. player_hand i stedet for dealer hand.
         print("You win!")
-        print(f"You have now {chip_stack} chips")
+        chip_stack = chip_stack + bet
+        print(f"You now have {chip_stack} chips")
+
     if dealer_score > player_score and dealer_score < 22:                               #Her fant jeg ut at jeg hadde feil tegnsetting etter "and dealer_score"
-        chip_stack.append(bet)
         print(f"Your hand is {player_hand} with a value of {player_score}")
         print(f"Dealer's hand is {dealer_hand} with a value of {dealer_score}")         #hadde feil variabel i printen. player_hand i stedet for dealer hand.
         print("Dealer wins!")
-        print(f"You have now {chip_stack} chips")
+        chip_stack = chip_stack - bet
+        print(f"You now have {chip_stack} chips")
+        if chip_stack == 0:
+            print("You're out of chips!")
     if player_score == dealer_score:                                                    #Og her fant jeg ut at jeg hadde feil variabel i statement'en. Jeg hadde først dealer_hand. Det var svært frustrerende.
         print(f"Your hand is {player_hand} with a value of {player_score}")
         print(f"Dealer's hand is {dealer_hand} with a value of {dealer_score}")
@@ -62,80 +67,182 @@ def print_results_hit(player_score, dealer_score):
     if dealer_score > 21:
         print(f"Your hand is {player_hand} with a value of {player_score}")
         print(f"The dealers visible card is a {dealer_hand[0]}, with a value of {bjm.get_card_value(dealer_hand[0])}")
+
     if player_score > dealer_score and player_score < 22:
         print(f"Your hand is {player_hand} with a value of {player_score}")
         print(f"The dealers visible card is a {dealer_hand[0]}, with a value of {bjm.get_card_value(dealer_hand[0])}")
+
     if dealer_score > player_score and dealer_score < 22:
         print(f"Your hand is {player_hand} with a value of {player_score}")
         print(f"The dealers visible card is a {dealer_hand[0]}, with a value of {bjm.get_card_value(dealer_hand[0])}")
+
     if player_score == dealer_score:
         print(f"Your hand is {player_hand} with a value of {player_score}")
         print(f"The dealers visible card is a {dealer_hand[0]}, with a value of {bjm.get_card_value(dealer_hand[0])}")
 
 
-'''deal_cards()
+# -------------------------------------------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------------------------------------------------
 
-player_score = bjm.calculate_hand_value(player_hand)
-dealer_score = bjm.calculate_hand_value(dealer_hand)'''
+
 
 while True:
     try:
         print("-------------------------------")
         print("Welcome to a game of blackjack!")
         print("-------------------------------")
+#1.6)
+        print(f"You have {chip_stack} chips.")
         bet = int(input("How many chips do you want to bet? \nBet:"))
-        if chip_stack > 0:
+        while chip_stack > 0 or chip_stack != 0:
             while chip_stack != 0:
                 player_hand = []
                 dealer_hand = []
-                if bet <= chip_stack and bet > 0:
+                if bet <= chip_stack: #and bet > 0:
                     deal_cards()
                     player_score = bjm.calculate_hand_value(player_hand)
                     dealer_score = bjm.calculate_hand_value(dealer_hand)
 
-                    #1.3
-                    while True: #player_input != "4":
+ # -------------------------------------------------------------------------------------------------------------------------------------------------
+ # -------------------------------------------------------------------------------------------------------------------------------------------------
+
+                    #1.3)
+
+                    while player_input != "4" or chip_stack > 0:
                         # 1.2 A)
                         if player_score == 21:
                             print("--------------------------------")
                             print("Blackjack! You won.")
-                            break
+                            chip_stack = int(chip_stack + bet)
+                            print(f"You now have {chip_stack} chips")
+                            print("--------------------------------")
+                            choice_input = input("Do you want to play another round? \n1. Yes \n2. No \nAnswer: ")
+                            if choice_input == "1":
+                                player_hand = []
+                                player_score = 0
+                                dealer_hand = []
+                                dealer_score = 0
+                                deck = bjm.get_new_shuffled_deck()
+                                bet = int(input("How many chips do you want to bet? \nBet:"))
+                                deal_cards()
+                                player_score = bjm.calculate_hand_value(player_hand)
+                                dealer_score = bjm.calculate_hand_value(dealer_hand)
+
+                            if choice_input == "2":
+                                print("Okey. Goodbye, and welcome back!")
+                                exit()
+
                         #1.2 B,C,D)
                         else:
                             print("--------------------------------")
-                            while player_input != "2" or player_score < 22:
-                                player_input = input("Do you want to:\n1. Hit \n2. Stand \n3. Restart \n4. Exit \nAnswer:")
+                            while True: #player_input != "4" and player_score <= 21:
+                                if chip_stack < 0:
+                                    print("You're out of chips, goodbye!")
+                                    exit()
+
+                                player_input = input("Do you want to:\n1. Hit \n2. Stand \n3. Restart Game \n4. Exit \nAnswer:")
                                 print("-------------------------------")
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------------------------------------------------
+
                                 if player_input == "1":
                                     print("You chose to Hit!")
                                     print("-------------------------------")
                                     player_hand.append(random.choice(list(deck)))
                                     deck.remove(player_hand[-1])
                                     player_score = bjm.calculate_hand_value(player_hand)
-                                    print_results_hit(player_score, dealer_score)
+                                    print_results_hit(player_score, dealer_score,)
                                     if player_score > 21:
+                                        chip_stack = int(chip_stack - bet)
                                         print(f"You now have {player_hand}  with a value of {player_score}")
                                         print("Busted! You got more than 21\nDealer wins!")
-                                        break
+                                        print(f"You now have {chip_stack} chips")
+                                    #if player_score > 21:
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------
 
                                 if player_input == "2":                                 #NOTETOSELF; Stand gjør pr. nå ingenting. 19.10
                                     print("You chose to stand!")
                                     print("-------------------------------")
                                     player_score = bjm.calculate_hand_value(player_hand)
                                     dealer_score = bjm.calculate_hand_value(dealer_hand)
+
                                     #1.4)
                                     while dealer_score < 17:
                                         print("dealer draws a card")
                                         dealer_hand.append(random.choice(list(deck)))
                                         deck.remove(dealer_hand[-1])
                                         dealer_score = bjm.calculate_hand_value(dealer_hand)
-                                    print_results_stand(player_score, dealer_score)
+                                    #chip_stack = int(chip_stack + bet)
+                                    print_results_stand(player_score, dealer_score, chip_stack)
+
+                                if chip_stack == 0:
+                                    print("You're out of chips")
+                                    choice_input = input("Do you want to restart the game? \n1. Yes \n2. No \nAnswer: ")
+                                    if choice_input == "1":
+                                        player_hand = []
+                                        player_score = 0
+                                        dealer_hand = []
+                                        dealer_score = 0
+                                        deck = bjm.get_new_shuffled_deck()
+                                        chip_stack = 5
+                                        print(f"You have {chip_stack} chips.")
+                                        bet = int(input("How many chips do you want to bet? \nBet:"))
+                                        deal_cards()
+                                        player_score = bjm.calculate_hand_value(player_hand)
+                                        dealer_score = bjm.calculate_hand_value(dealer_hand)
+                                        print_results_stand(player_score,dealer_score,chip_stack)
+                                    if choice_input == "2":
+                                        print("Goodbye!")
+                                        exit()
+
+                                    while chip_stack != 0:
+                                        choice_input = input("Do you want to play another round? \n1. Yes \n2. No \nAnswer: ")
+                                        if choice_input == "1":
+                                            player_hand = []
+                                            player_score = 0
+                                            dealer_hand = []
+                                            dealer_score = 0
+                                            chip_stack = chip_stack + bet
+                                            bet = 0
+                                            deck = bjm.get_new_shuffled_deck()
+                                            print(f"You now have {chip_stack} chips")
+                                            deal_cards()
+
+                                        if chip_stack == 0:
+                                            print("You're out of chips!")
+                                            choice_input = input( "Do you want to restart the game? \n1. Yes \n2. No \nAnswer: ")
+                                            if choice_input == "1":
+                                                player_hand = []
+                                                player_score = 0
+                                                dealer_hand = []
+                                                dealer_score = 0
+                                                deck = bjm.get_new_shuffled_deck()
+                                                bet = int(input("How many chips do you want to bet? \nBet:"))
+                                                deal_cards()
+
+                                            if choice_input == "2":
+                                                print("Goodbye!")
+                                                exit()
+                                        else:
+                                            bet = int(input("How many chips do you want to bet? \nBet:"))
+                                            deal_cards()
+
+                                        if choice_input == "2":
+                                            "Okey. Goodbye, and welcome back!"
+                                            exit()
+
+# -------------------------------------------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------------------------------------------------
 
                                 if player_input == "3":
                                     player_hand = []
                                     player_score = 0
                                     dealer_hand = []
                                     dealer_score = 0
+                                    chip_stack = 5
                                     deck = bjm.get_new_shuffled_deck()
                                     deal_cards()
 
@@ -143,4 +250,5 @@ while True:
                                     print("Goodbye!")
                                     exit()
     except ValueError:
-        print("Input type was wrong")
+        print("Input value was wrong")
+        exit()
